f2491ec70e887273852b88a22d114d0735d47e11
Kevin Harrington <kharrington@softroboticsinc.com>
2022-07-27 14:45:35 -0400

    adding stubs

--- fingerCad.groovy
+++ fingerCad.groovy
@@ -1,1 +1,24 @@
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+
+import eu.mihosoft.vrl.v3d.CSG
+
 //Your code here
+
+return new ICadGenerator() {
+
+	@Override
+	public ArrayList<CSG> generateCad(DHParameterKinematics dh, int linkIndex) {
+		ArrayList<CSG> parts = new ArrayList();
+		
+		return parts;
+	}
+
+	@Override
+	public ArrayList<CSG> generateBody(MobileBase base) {
+		ArrayList<CSG> parts = new ArrayList();
+		return parts;
+	}
+	
+}

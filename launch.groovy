69ed7cab10025f487605da61814ac7bbfa59fae8
Kevin Harrington <kharrington@softroboticsinc.com>
2022-07-27 13:47:55 -0400

    adding code for launching

--- launch.groovy
+++ launch.groovy
@@ -1,1 +1,13 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.creature.MobileBaseLoader
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+def gitURL = "https://github.com/madhephaestus/Fanuc_LR_Mate_200id_7L.git"
+
+def mockCMOD = ScriptingEngine.gitScriptRun(gitURL, "loadHardware.groovy")
+
+
+MobileBase fromGit = MobileBaseLoader.fromGit(gitURL, "Fanuc_LR_Mate_200id_7L.xml");
+fromGit.connect();
+simulator = fromGit
+				.getAllDHChains().get(0);
+simulator.homeAllLinks();
--- loadHardware.groovy
+++ loadHardware.groovy
@@ -1,1 +1,59 @@
-//Your code here
+import java.util.ArrayList;
+
+import com.neuronrobotics.sdk.addons.kinematics.AbstractLink;
+import com.neuronrobotics.sdk.addons.kinematics.INewLinkProvider;
+import com.neuronrobotics.sdk.addons.kinematics.LinkConfiguration;
+import com.neuronrobotics.sdk.addons.kinematics.LinkFactory;
+
+public class MockCMOD implements INewLinkProvider{
+	private static final double CLOSE_VLAUE = -3;
+
+	private static final double OPEN_ANGLE = 6.5;
+
+	ArrayList<AbstractLink> links = new ArrayList<>();
+	
+	private double angle = 0;
+
+	public MockCMOD() {
+		LinkFactory.addLinkProvider("CMOD", this);
+	}
+
+	public void setClose(boolean val) {
+		angle=val?OPEN_ANGLE:CLOSE_VLAUE;
+		for(int i=0;i<links.size();i++) {
+			links.get(i).fireLinkListener(angle);
+		}
+	}
+
+	@Override
+	public AbstractLink generate(LinkConfiguration lc) {
+		
+		AbstractLink abstractLink = new AbstractLink(lc) {
+			
+			@Override
+			public double getCurrentPosition() {
+				return angle;
+			}
+			
+			@Override
+			public void flushDevice(double v) {
+				angle=getTargetValue();
+			}
+			
+			@Override
+			public void flushAllDevice(double v) {
+				angle=getTargetValue();
+			}
+			
+			@Override
+			public void cacheTargetValueDevice() {
+				
+			}
+		};
+		links.add(abstractLink);
+		return abstractLink;
+	}
+
+}
+
+return new MockCMOD()

4ae843e5cbcb385ed664dd8b11af1f6dc2df8093
Kevin Harrington <kharrington@softroboticsinc.com>
2022-11-02 14:34:55 -0400

    reduced qrist set

--- DefaultDhSolver.groovy
+++ DefaultDhSolver.groovy
@@ -293,28 +293,9 @@ public class scriptJavaIKModel implements DhInverseSolver {
 		AbstractKinematicsNR kin = chain.kin;
 		// DecimalFormat df = new DecimalFormat("000.00");
 		double[] alt1 =option( calculated[0] - 180, -calculated[1], calculated[2] - 180 );
-		double[] calculated2 =option( calculated[0] + 360, calculated[1] + 360, calculated[2] + 360 );
-		double[] calculated3 =option( calculated[0] - 360, calculated[1] - 360, calculated[2] - 360 );
-		double[] alt2 =option( alt1[0] + 360, alt1[1] + 360, alt1[2] + 360 );
-		double[] alt3 =option( alt1[0] - 360, alt1[1] - 360, alt1[2] - 360 );
-		double[] calculated6 =option( calculated[0] - 360, calculated[1], calculated[2] );
-		double[] calculated7 =option( calculated[0] + 360, calculated[1], calculated[2] );
-		double[] als4 =option( alt1[0] - 360, alt1[1], alt1[2] );
-		double[] alt5 =option( alt1[0] + 360, alt1[1], alt1[2] );
-		
-		
-		
 		HashMap<double[], Double> scores = new HashMap<>();
 		score(calculated, current, scores, kin);
 		score(alt1, current, scores, kin);
-		score(calculated2, current, scores, kin);
-		score(calculated3, current, scores, kin);
-		score(alt2, current, scores, kin);
-		score(alt3, current, scores, kin);
-		score(calculated6, current, scores, kin);
-		score(calculated7, current, scores, kin);
-		score(als4, current, scores, kin);
-		score(alt5, current, scores, kin);
 		score(option( calculated[0] -180, -calculated[1], calculated[2]+180 ), current, scores, kin);
 		score(option( alt1[0] -180, alt1[1], -alt1[2]+180 ), current, scores, kin);
 		score(option( calculated[0] +180, -calculated[1], calculated[2]-180 ), current, scores, kin);
@@ -360,12 +341,6 @@ public class scriptJavaIKModel implements DhInverseSolver {
 			int i3 = i + 3;
 			calculated[i] = calculated[i] % 360;
 			
-//			if (calculated[i] > kin.getMaxEngineeringUnits(i3)) {
-//				return;
-//			}
-//			if (calculated[i] < kin.getMinEngineeringUnits(i3)) {
-//				return;
-//			}
 			double measure = current[i] - calculated[i];
 			if (Math.abs(measure) > Math.abs(delt)) {
 				delt = measure;
